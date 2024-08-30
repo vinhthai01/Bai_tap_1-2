a8fb715ae342d63e6c0f0532fdeb9acba73894d1
Le Vinh Thai <lethai150601@gmail.com>
2024-08-31 00:31:07 +0700

    UPDATE

--- Compiler_Macro/main.c
+++ Compiler_Macro/main.c
@@ -1,9 +1,29 @@
 #include <stdio.h>
 
+/*
+#define ARRAY(...)                                              \
+int array[] = {__VA_ARGS__};                                    \
+    int i = 0;                                                  \
+do {                                                            \
+    printf ("array[%d] = %d \n", i,array[i]);         clea             \
+    i++;                                                        \
+} while (i < sizeof(array)/sizeof(int))
+*/
 
-int main( int argc, char const *argv[]){
-    printf(" hello all");
-    
+#define ARRAY(...)                  \
+int array[] = {__VA_ARGS__};        \
+int i = 0;                          \
+for ( ; i < sizeof(array)/sizeof(int) ; i++){        \
+    printf ("array[%d] = %d\n", i,array[i]);         \
+}
+
+
+
+
+
+int main( int argc, char const *argv[] ){
+
+    ARRAY(1,5,3);
     return 0;
 }
 
